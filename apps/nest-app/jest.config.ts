b8a1fa4970bc3d95016fd25f3edfe81bfacae720
Hichchi Dev <hichchidev@gmail.com>
2024-12-04 01:09:50 +0530

    eslint

--- apps/nest-app/jest.config.ts
+++ apps/nest-app/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'nest-app',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/nest-app'
+    displayName: "nest-app",
+    preset: "../../jest.preset.js",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
+    },
+    moduleFileExtensions: ["ts", "js", "html"],
+    coverageDirectory: "../../coverage/apps/nest-app",
 };
