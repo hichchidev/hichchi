55185f7dca91cdb8d958148fa16d34bd79741261
Hichchi Dev <hichchidev@gmail.com>
2025-06-25 22:37:03 +0530

    Remove unused LoggerService initialization in AppController constructor.

--- apps/nest-app/src/app.controller.ts
+++ apps/nest-app/src/app.controller.ts
@@ -1,15 +1,9 @@
 import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./app.service";
-import { LoggerService } from "@hichchi/nest-core";
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
-        LoggerService.error(new Error("Error message"), {
-            message: "Error message long one",
-            context: this.constructor.name,
-        });
-    }
+    constructor(private readonly appService: AppService) {}
 
     @Get()
     getData(): { message: string } {
