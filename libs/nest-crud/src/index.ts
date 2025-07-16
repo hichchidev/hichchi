9604cc39875b9179f26ecb9fc035df6f62467445
Hichchi Dev <hichchidev@gmail.com>
2025-07-16 21:27:56 +0530

    Refactored `libs/nest-crud` module structure: reorganized files and updated imports for consistency and maintainability.

--- libs/nest-crud/src/index.ts
+++ libs/nest-crud/src/index.ts
@@ -1,4 +1,14 @@
 export { InjectRepository } from "@nestjs/typeorm";
 export { Repository } from "typeorm";
 
-export * from "./lib/index";
+export * from "./base";
+export * from "./decorators";
+export * from "./dtos";
+export * from "./enums";
+export * from "./constants";
+export * from "./classes";
+export * from "./services";
+export * from "./interfaces";
+export * from "./tokens";
+export * from "./types";
+export { HichchiCrudModule } from "./crud.module";
